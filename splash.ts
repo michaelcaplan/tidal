b4aeb30242feb1f860e9353e693c488fb370e40e
Michael Caplan <me@michaelcaplan.com>
2026-02-17 16:12:04 -0400

    moved slpash song to asset

---
+++ splash.ts
@@ -0,0 +1,12 @@
+class Splash {
+    constructor() {
+    }
+
+    public show() {
+        game.splash("Welcome to the Gym!", "Press A to start")
+    }
+
+    public hide() {
+        game.splash("hiding")
+    }
+}
